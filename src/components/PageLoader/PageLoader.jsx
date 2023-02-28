a257b0317e1bd6d19777c4a1f331c4315504763b
Evmenov Dmitry <dimitsky@icloud.com>
2023-02-28 14:16:57 +0400

    Добавил стили для плейсхолдера загрузки

--- src/components/PageLoader/PageLoader.jsx
+++ src/components/PageLoader/PageLoader.jsx
@@ -1,11 +1,17 @@
 import { createPortal } from "react-dom";
 
 import styles from './PageLoader.module.css';
+import loaderStyles from './loader.css';
 
 export default function PageLoader() {
     const domNode = window.document.body;
     const component = (
-        <div className={styles.wrap}></div>
+        <div className={styles.wrap}>
+            <div className={styles.inner}>
+                <div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            </div>
+        </div>
     );
+    
     return createPortal(component, domNode)
 }
